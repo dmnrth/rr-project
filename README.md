3885d8fac62e0efb76eaddb77a2ab488cdbf4b77
Damian Ślusarczyk <damian.slusarczyk1502@gmail.com>
2024-05-16 23:18:10 +0200

    Updated ARIMA functions to include GARCH, updated README.md
    - Added ARIMA-GARCH functions to arima_models.R.
    - Updated the logs to include information about convergence.
    - Added pararell computing to the model estimation.
    - Added ARIMA-GARCH estimation to 02_estimate_arima.qmd.
    - Updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Reproducible Research - Final Project
-Final project for the Reproducible Research course
+
+Final project for the Reproducible Research course in 2023/2024 spring semester, group members:
+
+-   Katarzyna Dybcio
+-   Quoc An Frejter
+-   Kamil Kashif
+-   Damian Ślusarczyk
+
+## :beginner: About
+
+WiP (what paper do we reproduce, why, etc)
+
+## :file_folder: File structure
+
+```         
+.
+├── functions
+│   └── arima_models.R
+├── input
+│   └── sp500_prices.rds
+├── logs
+│   └── ...
+├── 01_download_data.qmd
+├── 02_estimate_arima.qmd
+└── README.md
+```
+
+| No  | File name             | Details                                                            |
+|------------------------|------------------------|------------------------|
+| 1   | 01_download_data.qmd  | Downloading S&P500 price data, calculating descriptive statistics. |
+| 2   | 02_estimate_arima.qmd | Performing forecasts for the base case scenario.                   |
--- functions/arima_models.R
+++ functions/arima_models.R
@@ -1,6 +1,6 @@
 # Loading required packages
 
-packages <- c("forecast", "pbmcapply", "rugarch", "xts")
+packages <- c("doParallel", "foreach", "forecast", "pbmcapply", "rugarch", "xts")
 
 for (package in packages) {
   if (!require(package, character.only = TRUE)) {
@@ -53,12 +53,22 @@ fit_best_arima <- function(df,
   
   # Fitting the best model
   
+  converged <- TRUE
+  
+  # In case when the model fails to converge, the converged variable is set to false
+  
+  tryCatch({
   best_model <- Arima(df,
                       order = c(best_p, 1, best_q),
                       method = "ML",
                       optim.control = list(maxit = max_iterations))
+  }, warning = function(w) {
+    converged <- FALSE
+  })
 
-  return(best_model)
+  output = list(best_model, converged)
+  
+  return(output)
 }
 
 
@@ -71,6 +81,11 @@ arima_rolling_forecast <- function(prices,
                                    fit_best_arima_params = list(max_p = 5,
                                                                 max_q = 5,
                                                                 max_iterations = 500)) {
+  # Set up parallel processing
+  
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl)
+  
   # Prepare log file
   
   if (log) {
@@ -103,10 +118,12 @@ arima_rolling_forecast <- function(prices,
     
     # Fit best ARIMA model
     
-    model <- fit_best_arima(window,
-                            max_p = fit_best_arima_params$max_p,
-                            max_q = fit_best_arima_params$max_q,
-                            max_iterations = fit_best_arima_params$max_iterations)
+    fit_output <- fit_best_arima(window,
+                                 max_p = fit_best_arima_params$max_p,
+                                 max_q = fit_best_arima_params$max_q,
+                                 max_iterations = fit_best_arima_params$max_iterations)
+    
+    model <- fit_output[[1]]
     
     # Perform one day ahead forecast
     
@@ -120,7 +137,8 @@ arima_rolling_forecast <- function(prices,
       log_line <- paste0(format(Sys.time(), "[%Y-%m-%dT%H:%M:%S]"),
                          " Estimation window: ", index(prices[i - estimation_window_length]), " - ", index(prices[i - 1]),
                          ", Best order: (", arimaorder(model)[1], ", 1, ", arimaorder(model)[3],
-                         "), AIC: ", round(model$aic, 2))
+                         "), AIC: ", round(model$aic, 2),
+                         ifelse(fit_output[[2]], ", Final model converged", "Final model FAILED to converge"))
       
       writeLines(log_line, con = file_con)
     }
@@ -135,7 +153,182 @@ arima_rolling_forecast <- function(prices,
   if (progress_bar) {close(pb)}
   if (log) {close(file_con)}
   
+  # Stop cluster
+  
+  stopCluster(cl)
+  
   return(forecasts)
 }
 
-# TODO: Add ARIMA-GARCH functions
+
+# Function to find and fit the best ARIMA-GARCH model
+
+fit_best_arima_garch <- function(df,
+                                 max_p = 5,
+                                 max_q = 5,
+                                 max_iterations = 100000) {
+  p_orders <- 0:max_p
+  q_orders <- 0:max_q
+  
+  # Create order search space (excluding (0, 1, 0))
+  
+  orders <- expand.grid(p = p_orders, q = q_orders)
+  orders <- orders[!(orders$p == 0 & orders$q == 0),]
+  
+  # Initialize variables for best orders and best AIC
+  
+  best_p <- NULL
+  best_q <- NULL
+  best_aic <- Inf
+  
+  for (i in 1:nrow(orders)) {
+    p <- orders$p[i]
+    q <- orders$q[i]
+    
+    # Fitting ARIMA(p, 1, q)-GARCH(1, 1) model
+    
+    spec <- ugarchspec(variance.model = list(model = "sGARCH",
+                                             garchOrder = c(1, 1)),
+                       mean.model = list(armaOrder = c(p, q),
+                                         include.mean = TRUE))
+    
+    model <- ugarchfit(spec = spec,
+                       data = df,
+                       solver = "hybrid",
+                       solver.control = list(maxeval = max_iterations,
+                                             ftol_rel = 1e-6,
+                                             xtol_rel = 1e-4))
+    
+    # Update best parameters if AIC is better
+    
+    if (infocriteria(model)[1] < best_aic) {
+      best_p <- p
+      best_q <- q
+      best_aic <- infocriteria(model)[1]
+    }
+  }
+  
+  # Fitting the best model
+  
+  converged <- TRUE
+  
+  best_spec <- ugarchspec(variance.model = list(model = "sGARCH",
+                                           garchOrder = c(1, 1)),
+                          mean.model = list(armaOrder = c(best_p, best_q),
+                                            include.mean = TRUE))
+  
+  # In case when the model fails to converge, the converged variable is set to false
+  
+  tryCatch({
+  best_model <- ugarchfit(spec = best_spec,
+                          data = df,
+                          solver.control = list(maxit = max_iterations))
+  }, warning = function(w) {
+    converged <- FALSE
+  })
+  
+  output = list(best_model, converged)
+  
+  return(output)
+}
+
+
+arima_garch_rolling_forecast <- function(prices,
+                                   estimation_start_date,
+                                   estimation_end_date,
+                                   estimation_window_length = 1000,
+                                   seed = 1,
+                                   log = TRUE,
+                                   progress_bar = TRUE,
+                                   fit_best_arima_garch_params = list(max_p = 5,
+                                                                      max_q = 5,
+                                                                      max_iterations = 100000),
+                                   ugarchboot_params = list(method = c("Partial","Full")[1],
+                                                            n.bootpred = 100,
+                                                            n.bootfit = 500)) {
+  # Set up parallel processing
+  
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl)
+  
+  # Prepare log file
+  
+  if (log) {
+    log_name <- paste0(format(Sys.time(), "%Y%m%d_%H%M%S"), "_ARIMA-GARCH.txt")
+    path <- paste0("logs/", log_name)
+    file.create(path)
+    file_con <- file(path, open = "a")
+  }
+  
+  # Initialize variables for rolling forecast
+  
+  estimation_start_date <- as.Date(estimation_start_date)
+  initial_t <- which(index(prices) == estimation_start_date)
+  estimation_end_date <- as.Date(estimation_end_date)
+  end_t <- which(index(prices) == estimation_end_date)
+  forecasts <- xts()
+  
+  # Initialize progress bar
+  
+  if (progress_bar) {
+    pb <- progressBar(min = initial_t, max = end_t)
+  }
+  
+  # Perform rolling forecast
+  
+  for (i in initial_t:end_t) {
+    # Prepare data
+    
+    window <- prices[(i - estimation_window_length):(i - 1)]
+    
+    # Fit best ARIMA-GARCH model
+    
+    fit_output <- fit_best_arima_garch(window,
+                                       max_p = fit_best_arima_garch_params$max_p,
+                                       max_q = fit_best_arima_garch_params$max_q,
+                                       max_iterations = fit_best_arima_garch_params$max_iterations)
+    model <- fit_output[[1]]
+    
+    # Perform one day ahead forecast
+    
+    arima_garch_forecast <- ugarchboot(model,
+                                       method = ugarchboot_params$method,
+                                       n.ahead = 1,
+                                       n.bootpred = ugarchboot_params$n.bootpred,
+                                       n.bootfit = ugarchboot_params$n.bootfit,
+                                       rseed = seq(seed + i, 
+                                                   seed + i +
+                                                     ugarchboot_params$n.bootpred +
+                                                     ugarchboot_params$n.bootfit)) # seed must be a vector of length n.bootpred + n.bootfit
+    new_row <- xts(arima_garch_forecast@forc@forecast$seriesFor, index(prices)[i])
+    forecasts <- rbind(forecasts, new_row)
+    
+    # Log
+    
+    if (log) {
+      log_line <- paste0(format(Sys.time(), "[%Y-%m-%dT%H:%M:%S]"),
+                         " Estimation window: ", index(prices[i - estimation_window_length]), " - ", index(prices[i - 1]),
+                         ", Best order: (", model@model$modelinc['ar'], ", 1, ", model@model$modelinc['ma'],
+                         ")-(1, 1), AIC: ", infocriteria(model)[1],
+                         ifelse(fit_output[[2]], ", Final model converged", "Final model FAILED to converge"))
+      
+      writeLines(log_line, con = file_con)
+    }
+    
+    # Update progress bar
+    
+    if (progress_bar) {
+      setTxtProgressBar(pb, i)
+    }
+  }
+  
+  if (progress_bar) {close(pb)}
+  if (log) {close(file_con)}
+
+  
+  # Stop cluster
+  
+  stopCluster(cl)
+  
+  return(forecasts)
+}
